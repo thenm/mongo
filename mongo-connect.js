69ba3b9dc757137743086561250695d5c71d023b
Nitin Maul <intern.nitin.maul@firstcry.com>
2018-01-18 14:16:04 +0530

    mongo-connection

---
+++ mongo-connect.js
@@ -0,0 +1,10 @@
+// var MongoClient = require('mongodb').MongoClient;
+// ES6 Object Destruction
+var {MongoClient} = require('mongodb');
+var url = 'mongodb://localhost:27017/demo-db';
+
+MongoClient.connect(url, (err, db) => {
+  if(err) return console.log('Unable to connect to the DB');
+  console.log('Connnected to DB');
+  db.close();
+});
